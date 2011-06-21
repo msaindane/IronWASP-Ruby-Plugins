cd0db7db19b86acab4b32cb277b3cbb0fc9c4270
Manish Saindane <manish@andlabs.org>
2011-06-21 22:03:59 +0100

    Added plugin to check for Basic Authentication Added plugin to perform analysis on crossdomain.xml files and find security issues

---
+++ passive/BasicAuthentication.rb
@@ -0,0 +1,30 @@
+include Iron
+
+class BasicAuthentication < PassivePlugin
+    def Check(ironsess, results)
+        if ironsess.Response.headers.has('WWW-Authenticate') and !ironsess.Request.ssl
+            auth_method = ironsess.Response.headers.get('WWW-Authenticate')
+            report_basic_auth(ironsess, auth_method, results) if auth_method =~ /basic/i
+        end
+    end
+    
+    def report_basic_auth(ironsess, auth_method, results)
+        plugin_result = PluginResult.new(ironsess.Request.host)
+        plugin_result.title = "Insecure Basic Authentication used"
+        plugin_result.summary = "The server uses Basic Authentication over insecure channel. Basic Authentication encodes the credentials using Base64 which can be easily decoded if captured over insecure channel."
+        plugin_result.triggers.add('',ironsess.Request, auth_method, ironsess.Response)
+        plugin_result.result_type = PluginResultType.vulnerability
+        plugin_result.confidence = PluginResultConfidence.high
+        plugin_result.severity = PluginResultSeverity.high
+        plugin_result.uniqueness_string = "BasicAuthentication|vulnerability|high|authorization|#{ironsess.Request.host}"
+        results.add(plugin_result)
+    end
+end
+
+p = BasicAuthentication.new
+p.name = "Basic Auth Check"
+p.description = "This plugin checks for the use of Basic Authentication over insecure channels."
+p.file_name = "BasicAuthentication.rb"
+p.calling_state = PluginCallingState.before_interception
+p.works_on = PluginWorksOn.response
+PassivePlugin.add(p)
---
+++ passive/CrossDomainXmlPolicyAnalysis.rb
@@ -0,0 +1,60 @@
+# Author: Manish Saindane
+
+include Iron
+
+class CrossDomainXmlPolicyAnalysis < PassivePlugin
+    def Check(ironsess, results)
+        if ironsess.Request.url =~ /crossdomain.xml/i
+            check_allowed_domains(ironsess, results)
+        end
+    end
+    
+    def check_allowed_domains(ironsess, results)
+        url = ironsess.Request.url
+        bs = ironsess.Response.body_string
+        xml_doc = HTML.new(bs)
+        allowed_domain_tags = xml_doc.get_nodes('allow-access-from', 'domain')
+        unless allowed_domain_tags.nil?
+            allowed_domain_tags.each do |node|
+                domain_value = node.get_attribute_value('domain', ''.to_clr_string)
+                report_allowed_domains(ironsess, node.outer_html, results) if domain_value =~ /^\*$/
+                if ironsess.Request.ssl
+                    secure_flag = node.get_attribute_value('secure', ''.to_clr_string)
+                    report_secure_flag(ironsess, node.outer_html, results) if secure_flag =~ /false/
+                end
+            end
+        end
+    end
+    
+    def report_allowed_domains(ironsess, node, results)
+        plugin_result = PluginResult.new(ironsess.Request.host)
+        plugin_result.title = "Wildcard domain set on Cross-domain policy"
+        plugin_result.summary = "Setting wildcard (*) domains would allow access to documents originating from any domain."
+        plugin_result.triggers.add('',ironsess.Request, node, ironsess.Response)
+        plugin_result.result_type = PluginResultType.vulnerability
+        plugin_result.confidence = PluginResultConfidence.high
+        plugin_result.severity = PluginResultSeverity.high
+        plugin_result.uniqueness_string = "CrossDomainXmlPolicyAnalysis|vulnerability|high|cross-domain-policy|#{ironsess.Request.host}|open domain|#{node}"
+        results.add(plugin_result)
+    end
+    
+    def report_secure_flag(ironsess, node, results)
+        plugin_result = PluginResult.new(ironsess.Request.host)
+        plugin_result.title = "Secure flag set to false on Cross-domain policy"
+        plugin_result.summary = "The web application permits SWF files on a non-HTTPS server to load data from this HTTPS server. Setting the secure attribute to false could compromise the security offered by HTTPS. In particular, setting this attribute to false opens secure content to snooping and spoofing attacks."
+        plugin_result.triggers.add('',ironsess.Request, node, ironsess.Response)
+        plugin_result.result_type = PluginResultType.vulnerability
+        plugin_result.confidence = PluginResultConfidence.high
+        plugin_result.severity = PluginResultSeverity.high
+        plugin_result.uniqueness_string = "CrossDomainXmlPolicyAnalysis|vulnerability|high|cross-domain-policy|#{ironsess.Request.host}|secure flag|#{node}"
+        results.add(plugin_result)
+    end
+end
+
+p = CrossDomainXmlPolicyAnalysis.new
+p.name = "Cross-domain XML policy analysis"
+p.description = "This plugin analyzes the cross-domain policy for the web server and reports vulnerabilities."
+p.file_name = "CrossDomainXmlPolicyAnalysis.rb"
+p.calling_state = PluginCallingState.before_interception
+p.works_on = PluginWorksOn.response
+PassivePlugin.add(p)
